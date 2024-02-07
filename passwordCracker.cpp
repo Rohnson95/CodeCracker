b684122d570df6ce4b6f86b17b7821484e5e1867
Robert Johnson <rohnson95@gmail.com>
2024-02-07 07:56:54 +0100

    removed unessecary code

--- passwordCracker.cpp
+++ passwordCracker.cpp
@@ -9,35 +9,6 @@
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 #include "passwordCracker.h"
-// struct PasswordHashPair
-// {
-//     std::string password;
-//     std::string hash;
-// };
-
-// std::string sha256(const std::string &input)
-// {
-//     EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
-//     const EVP_MD *md = EVP_sha256();
-//     unsigned char hash[EVP_MAX_MD_SIZE];
-//     unsigned int hash_len;
-
-//     EVP_DigestInit_ex(mdctx, md, nullptr);
-//     EVP_DigestUpdate(mdctx, input.c_str(), input.length());
-//     EVP_DigestFinal_ex(mdctx, hash, &hash_len);
-//     EVP_MD_CTX_free(mdctx);
-
-//     // Convert the binary hash to a hex string
-//     std::string hashedPassword;
-//     for (unsigned int i = 0; i < hash_len; ++i)
-//     {
-//         char hex[3];
-//         sprintf(hex, "%02x", hash[i]);
-//         hashedPassword += hex;
-//     }
-
-//     return hashedPassword;
-// }
 
 bool compareByHash(const PasswordHashPair &a, const PasswordHashPair &b)
 {
@@ -81,13 +52,6 @@ int hashPasswords()
 
     // return 0;
 }
-// TODO: Make a brute force checker for the sorted_common_passwords.txt file to see if you can get the plaintext password from the hash
-// This should be doable by taking a hash input from the user and checking against the file for a match, and then printing the plaintext password if there is a match
-
-// create a function that converts a hash to a plaintext password
-// create a function that takes a hash and checks it against the sorted_common_passwords.txt file
-// if there is a match, print the plaintext password
-// if there is no match, print "Password not found"
 
 int mainCracker()
 {
